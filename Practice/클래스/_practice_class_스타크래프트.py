d71c049cdbfd14a8c2803e4bc935d8010a5e7032
HyungJun Jo <hj.jo@haeahn.com>
2020-12-14 00:06:28 +0900

    Practice 이름 변경

--- Practice/클래스/_practice_class_스타크래프트.py
+++ Practice/클래스/_practice_class_스타크래프트.py
@@ -1,3 +1,5 @@
+from random import *
+
 
 class Unit:
     def __init__(self, name, hp, speed):           # 생성자
@@ -121,3 +123,38 @@ t2 = Tank()
 w1 = Wraith()
 
 # 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seize_developed = True
+print("[알림] 탱크 시즈모드 개발이 완료되었습니다.")
+
+# 공격 모드 준비
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.steampack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+# 전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5,21)) # 공격은 랜덤을 받음 (5~20)
+
+# 게임 종료
+game_over()
